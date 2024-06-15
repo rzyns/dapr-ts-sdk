ac9d53185b5b8dd85afa0559ff6676fa0ea5bfb3
Janusz Dziurzyński <janusz@forserial.org>
2024-06-15 20:44:17 +0000

    Tweak devcontainer

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+# install bun
+# curl -fsSL https://bun.sh/install | bash
 
+# install dapr
+# wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+
+# install eza
 wget -O - -c https://github.com/eza-community/eza/releases/download/v0.18.17/eza_x86_64-unknown-linux-gnu.tar.gz | tar xzv
 sudo chmod +x eza
 sudo chown root:root eza
